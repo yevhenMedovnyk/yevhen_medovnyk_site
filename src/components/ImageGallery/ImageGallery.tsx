27298fb12f0107fbee50297c2e6c7aad59cdf7df
Yevhen Medovnyk <yevhenmed@gmail.com>
2025-06-08 18:27:39 +0300

    change lazy-image style

--- src/components/ImageGallery/ImageGallery.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './ImageGallery.module.scss';
-
-const ImageGallery: React.FC = () => {
-	return <div className={s.container}>ImageGallery</div>;
-};
-
-export default ImageGallery;
