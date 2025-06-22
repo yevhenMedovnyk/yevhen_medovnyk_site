0b7a598a02f1ba057dea8735baea8196371cc211
Yevhen Medovnyk <yevhenmed@gmail.com>
2025-06-22 18:24:41 +0300

    create toast components

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,12 +6,14 @@ import s from './Layout.module.scss';
 import Footer from '../Footer/Footer';
 import MainTitle from '../MainTitle/MainTitle';
 import { useScrollToTopOnRouteChange } from '../../hooks/useScrollToTopOnRouteChange';
+import { Toaster } from 'sonner';
 
 const Layout = () => {
 	useScrollToTopOnRouteChange();
 
 	return (
 		<div className={s.LayoutContainer}>
+			<Toaster duration={2000} />
 			<Header />
 			<MainTitle />
 			<main className={s.main}>
